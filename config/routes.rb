d1cbadb740b1d2efba64754998190c4342d2701c
Tristan Toye <tristantoye@gmail.com>
2019-12-31 17:14:58 -0500

    ➕ Installing webpack

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'home/index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
 

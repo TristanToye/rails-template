aacbc0ad816149994e35c6bd2f4231ef97309c7e
Tristan Toye <tristantoye@gmail.com>
2020-01-02 18:14:20 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'home/index'
-  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
